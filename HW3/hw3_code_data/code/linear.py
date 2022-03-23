2121aa9b0498f249fb5579be72f75e335984283a
Chinmay Garg <64963908+nefario7@users.noreply.github.com>
2022-03-23 00:48:47 -0400

    create_linear_system complete

--- HW3/hw3_code_data/code/linear.py
+++ HW3/hw3_code_data/code/linear.py
@@ -1,7 +1,7 @@
-'''
+"""
     Initially written by Ming Hsiao in MATLAB
     Rewritten in Python by Wei Dong (weidong@andrew.cmu.edu), 2021
-'''
+"""
 
 import time
 import numpy as np
@@ -12,10 +12,12 @@ import matplotlib.pyplot as plt
 from solvers import *
 from utils import *
 
+from PIL import Image as im
+import pandas as pd
 
-def create_linear_system(odoms, observations, sigma_odom, sigma_observation,
-                         n_poses, n_landmarks):
-    '''
+
+def create_linear_system(odoms, observations, sigma_odom, sigma_observation, n_poses, n_landmarks):
+    """
     \param odoms Odometry measurements between i and i+1 in the global coordinate system. Shape: (n_odom, 2).
     \param observations Landmark measurements between pose i and landmark j in the global coordinate system. Shape: (n_obs, 4).
     \param sigma_odom Shared covariance matrix of odometry measurements. Shape: (2, 2).
@@ -25,7 +27,7 @@ def create_linear_system(odoms, observations, sigma_odom, sigma_observation,
     \return b (M, ) Residual vector.
     where M = (n_odom + 1) * 2 + n_obs * 2, total rows of measurements.
           N = n_poses * 2 + n_landmarks * 2, length of the state vector.
-    '''
+    """
 
     n_odom = len(odoms)
     n_obs = len(observations)
@@ -34,68 +36,80 @@ def create_linear_system(odoms, observations, sigma_odom, sigma_observation,
     N = n_poses * 2 + n_landmarks * 2
 
     A = np.zeros((M, N))
-    b = np.zeros((M, ))
+    b = np.zeros((M,))
 
     # Prepare Sigma^{-1/2}.
     sqrt_inv_odom = np.linalg.inv(scipy.linalg.sqrtm(sigma_odom))
     sqrt_inv_obs = np.linalg.inv(scipy.linalg.sqrtm(sigma_observation))
 
+    H = np.array([[-1, 0, 1, 0], [0, -1, 0, 1]])
     # TODO: First fill in the prior to anchor the 1st pose at (0, 0)
+    A[:2, :2] = sqrt_inv_odom
 
     # TODO: Then fill in odometry measurements
+    X0 = np.zeros((1, 2), dtype=np.float32)
+    for i in range(1, n_odom):
+        x = 2 * i
+        y = 2 * (i - 1)
+        A[x : x + 2, y : y + 4] = sqrt_inv_odom @ H.copy()
+        b[x : x + 2] = sqrt_inv_odom @ (odoms[i] - odoms[i - 1]).T
 
     # TODO: Then fill in landmark measurements
+    pose_idx = observations[:, 1].astype("int64")
+    land_idx = observations[:, 2].astype("int64")
+    for p, l in zip(pose_idx, land_idx):
+        i = (n_odom + 1 + l) * 2 - 1
+        ip = 2 * p
+        il = (n_poses + l) * 2 - 1
+        # print(f"Pose {p} and Landmark {l} : ({i},{ip}) and ({i},{il})")
+        A[i : i + 2, ip : ip + 2] = sqrt_inv_obs @ H[:, :2].copy()
+        A[i : i + 2, il : il + 2] = sqrt_inv_obs @ H[:, 2:].copy()
+        b[i : i + 2] = sqrt_inv_obs @ (observations[l, 2:] - odoms[p]).T
 
+    plt.imshow(A, interpolation="nearest")
+    plt.savefig("a_matrix.png")
     return csr_matrix(A), b
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument('data', help='path to npz file')
+    parser.add_argument("data", help="path to npz file")
     parser.add_argument(
-        '--method',
-        nargs='+',
-        choices=['default', 'pinv', 'qr', 'lu', 'qr_colamd', 'lu_colamd'],
-        default=['default'],
-        help='method')
-    parser.add_argument(
-        '--repeats',
-        type=int,
-        default=1,
-        help=
-        'Number of repeats in evaluation efficiency. Increase to ensure stablity.'
+        "--method", nargs="+", choices=["default", "pinv", "qr", "lu", "qr_colamd", "lu_colamd"], default=["default"], help="method"
     )
+    parser.add_argument("--repeats", type=int, default=1, help="Number of repeats in evaluation efficiency. Increase to ensure stablity.")
     args = parser.parse_args()
 
     data = np.load(args.data)
-
+    print("Loaded Data!")
     # Plot gt trajectory and landmarks for a sanity check.
-    gt_traj = data['gt_traj']
-    gt_landmarks = data['gt_landmarks']
-    plt.plot(gt_traj[:, 0], gt_traj[:, 1], 'b-', label='gt trajectory')
-    plt.scatter(gt_landmarks[:, 0],
-                gt_landmarks[:, 1],
-                c='b',
-                marker='+',
-                label='gt landmarks')
+    gt_traj = data["gt_traj"]
+    gt_landmarks = data["gt_landmarks"]
+    plt.plot(gt_traj[:, 0], gt_traj[:, 1], "b-", label="gt trajectory")
+    plt.scatter(gt_landmarks[:, 0], gt_landmarks[:, 1], c="b", marker="+", label="gt landmarks")
     plt.legend()
     plt.show()
+    plt.savefig("init.png")
 
     n_poses = len(gt_traj)
     n_landmarks = len(gt_landmarks)
 
-    odoms = data['odom']
-    observations = data['observations']
-    sigma_odom = data['sigma_odom']
-    sigma_landmark = data['sigma_landmark']
+    odoms = data["odom"]
+    observations = data["observations"]
+    sigma_odom = data["sigma_odom"]
+    sigma_landmark = data["sigma_landmark"]
+
+    print(n_poses, n_landmarks)
+    print(odoms.shape, observations.shape, sigma_odom.shape, sigma_landmark.shape)
+    # print(odoms, observations, sigma_odom, sigma_landmark)
 
     # Build a linear system
-    A, b = create_linear_system(odoms, observations, sigma_odom,
-                                sigma_landmark, n_poses, n_landmarks)
+    print("Building Linear System")
+    A, b = create_linear_system(odoms, observations, sigma_odom, sigma_landmark, n_poses, n_landmarks)
 
     # Solve with the selected method
     for method in args.method:
-        print(f'Applying {method}')
+        print(f"Applying {method}")
 
         total_time = 0
         total_iters = args.repeats
@@ -104,7 +118,7 @@ if __name__ == '__main__':
             x, R = solve(A, b, method)
             end = time.time()
             total_time += end - start
-        print(f'{method} takes {total_time / total_iters}s on average')
+        print(f"{method} takes {total_time / total_iters}s on average")
 
         if R is not None:
             plt.spy(R)
--- HW3/hw3_code_data/code/solvers.py
+++ HW3/hw3_code_data/code/solvers.py
@@ -1,7 +1,7 @@
-'''
+"""
     Initially written by Ming Hsiao in MATLAB
     Rewritten in Python by Wei Dong (weidong@andrew.cmu.edu), 2021
-'''
+"""
 
 from scipy.sparse import csc_matrix, eye
 from scipy.sparse.linalg import inv, splu, spsolve, spsolve_triangular
@@ -12,6 +12,7 @@ import matplotlib.pyplot as plt
 
 def solve_default(A, b):
     from scipy.sparse.linalg import spsolve
+
     x = spsolve(A.T @ A, A.T @ b)
     return x, None
 
@@ -19,7 +20,9 @@ def solve_default(A, b):
 def solve_pinv(A, b):
     # TODO: return x s.t. Ax = b using pseudo inverse.
     N = A.shape[1]
-    x = np.zeros((N, ))
+    x = np.zeros((N,))
+
+    x = scipy.sparse.linalg.inv(A.T @ A) @ A.T @ b
     return x, None
 
 
@@ -27,8 +30,12 @@ def solve_lu(A, b):
     # TODO: return x, U s.t. Ax = b, and A = LU with LU decomposition.
     # https://docs.scipy.org/doc/scipy/reference/generated/scipy.sparse.linalg.splu.html
     N = A.shape[1]
-    x = np.zeros((N, ))
+    x = np.zeros((N,))
     U = eye(N)
+
+    A_prime = splu(A, premc_spec="NATURAL")
+    x = A_prime.solve(b)
+
     return x, U
 
 
@@ -36,8 +43,12 @@ def solve_lu_colamd(A, b):
     # TODO: return x, U s.t. Ax = b, and Permutation_rows A Permutration_cols = LU with reordered LU decomposition.
     # https://docs.scipy.org/doc/scipy/reference/generated/scipy.sparse.linalg.splu.html
     N = A.shape[1]
-    x = np.zeros((N, ))
+    x = np.zeros((N,))
     U = eye(N)
+
+    A_prime = splu(A, premc_spec="COLAMD")
+    x = A_prime.solve(b)
+
     return x, U
 
 
@@ -45,8 +56,12 @@ def solve_qr(A, b):
     # TODO: return x, R s.t. Ax = b, and |Ax - b|^2 = |Rx - d|^2 + |e|^2
     # https://github.com/theNded/PySPQR
     N = A.shape[1]
-    x = np.zeros((N, ))
+    x = np.zeros((N,))
     R = eye(N)
+
+    Z, R, E, rank = rz()
+
+    x = spsolve_triangular
     return x, R
 
 
@@ -54,26 +69,26 @@ def solve_qr_colamd(A, b):
     # TODO: return x, R s.t. Ax = b, and |Ax - b|^2 = |R E^T x - d|^2 + |e|^2, with reordered QR decomposition (E is the permutation matrix).
     # https://github.com/theNded/PySPQR
     N = A.shape[1]
-    x = np.zeros((N, ))
+    x = np.zeros((N,))
     R = eye(N)
     return x, R
 
 
-def solve(A, b, method='default'):
-    '''
+def solve(A, b, method="default"):
+    """
     \param A (M, N) Jacobian matirx
     \param b (M, 1) residual vector
     \return x (N, 1) state vector obtained by solving Ax = b.
-    '''
+    """
     M, N = A.shape
 
     fn_map = {
-        'default': solve_default,
-        'pinv': solve_pinv,
-        'lu': solve_lu,
-        'qr': solve_qr,
-        'lu_colamd': solve_lu_colamd,
-        'qr_colamd': solve_qr_colamd,
+        "default": solve_default,
+        "pinv": solve_pinv,
+        "lu": solve_lu,
+        "qr": solve_qr,
+        "lu_colamd": solve_lu_colamd,
+        "qr_colamd": solve_qr_colamd,
     }
 
     return fn_map[method](A, b)
